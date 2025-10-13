a13774daddddd0b7f907d2166d3c174e1f19d3f3
Andy Wickert <andy@northernwidget.com>
2025-10-13 09:55:50 -0500

    Generate new Apis class: new name + enable arbitrarily large number of LiDAR readings.

---
+++ src/Apis.cpp
@@ -0,0 +1,141 @@
+#include "Apis.h"
+
+Apis::Apis()
+{
+}
+
+void Apis::begin(uint8_t ADR_, uint8_t Sensitivity)
+{
+    // Set address and sensitivity
+    ADR = ADR_;
+    Wire.begin();
+    Wire.beginTransmission(ADR_);
+    Wire.write(0x01);
+    Wire.write(Sensitivity);
+    Wire.endTransmission();
+}
+
+bool Apis::updateMeasurements(){
+
+    // Time for cap to settle or something
+    delay(200);
+
+    // Some of Bobby's unimplemented code to time out if a reading is not taken
+    //const unsigned long timeout = 200; //Wait up to 200ms for a new reading
+    //unsigned long localTime = millis(); //Keep track of local time for timeout
+
+    // Temporary values and arrays to hold data during I2C parsing.
+    // Initialize at 0 as default failure value
+    // Perhaps we should reset these to 0 within the loop?
+    uint8_t Data1 = 0;
+    uint8_t Data2 = 0;
+    int16_t DataSet[7];
+    
+    for(int i = 0; i < 7; i++) {
+          Wire.beginTransmission(ADR);
+          Wire.write(2*i + 2);
+          Wire.endTransmission();
+          Wire.requestFrom(ADR, 1);
+          Data1 = Wire.read();
+
+          Wire.beginTransmission(ADR);
+          Wire.write(2*i + 3);
+          Wire.endTransmission();
+          Wire.requestFrom(ADR, 1);
+          Data2 = Wire.read();
+          
+          // Store data in array
+          // Could save memory by replacing this with if/else
+          // for different individual variables
+          DataSet[i] = ((Data2 << 8) | Data1);
+    }
+
+    // Takes up memory but makes code easier to read. Could be more efficient.
+    // Perhaps use pointers, or just comments?
+  
+    // Measured laser range in centimeters
+    Range = DataSet[0];
+
+    //Convert g vals to floats for math
+    float gX = DataSet[1];
+    float gY = DataSet[2];
+    float gZ = DataSet[3];
+
+    float OffsetX = DataSet[4];
+    float OffsetY = DataSet[5];
+    float OffsetZ = DataSet[6];
+
+    // Calculate roll/pitch and parse errors
+    // Error return value = -9999.
+
+    // Errors in range if <0
+    if (Range < 0) {
+        Range = -9999;
+    }
+
+    // Errors in g if all -1
+    if(gX == gY && gX == gZ && gX == -1) {  
+        Pitch = -9999;
+        Roll = -9999;
+    }
+    // If no errors, continue!
+    // Orientation: roll and pitch
+    else if( OffsetX == OffsetY && OffsetX == OffsetZ && OffsetX == 0 ) {
+        // Do not include angle calc for offset when offset is 0
+        // (Results in NAN value)
+        Pitch = atan(-gX/gZ)*180./3.14;
+        Roll = atan( gY / (sqrt( pow(gX, 2) + pow(gZ, 2) )) )*180./3.14;
+    }
+    else {
+        Pitch = ( atan(-gX/gZ) - atan(-OffsetX/OffsetZ) ) * 180./3.14;
+        Roll = ( atan(gY / (sqrt( pow(gX, 2) + pow(gZ, 2) )) ) - \
+                 - atan(OffsetY \
+                         / (sqrt( pow(OffsetX, 2) + pow(OffsetZ, 2) )) ) ) \
+                 * 180./3.14;
+    }
+    
+    if ( (Range == -9999) || (Roll == -9999) || (Pitch == -9999) ){
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
+float Apis::getRoll() {
+    // Roll in degrees
+    return Roll;
+}
+
+float Apis::getPitch() {
+    // Pitch in degrees
+    return Pitch;
+}
+
+float Apis::getRange() {
+    // Distance in cm
+    return Range;
+}
+
+String Apis::getString(bool takeNewReadings) {
+    if (takeNewReadings) {
+        updateMeasurements();
+    }
+    return String(Range) + "," + String(Pitch) + "," + String(Roll) + ",";
+}
+
+String Apis::getHeader() {
+    return "Range [cm],Pitch [deg],Roll [deg],";
+}
+
+// Although Arduino standards are for functions to be camelCase, there is a 
+// legacy of CapitalCase. Therefore, to not break backwards compatibility:
+
+String Apis::GetString(bool takeNewReadings) {
+    return getString(takeNewReadings);
+}
+
+String Apis::GetHeader() {
+    return getHeader();
+}
+
---
+++ src/Apis.h
@@ -0,0 +1,149 @@
+/******************************************************************************
+Apis.h
+
+Library for the Apis interface board for a LiDAR Lite unit.
+
+Andrew Wickert
+Based on code by Bobby Schulz
+In particular: https://github.com/NorthernWidget-Skunkworks/Project-Symbiont-LiDAR/tree/master/Software/LiDARLite_I2CParse
+and based loosely around Bobby Schulz' general functions for sensor libraries.
+
+Started 2020.05.01
+Hardware located at:
+https://github.com/NorthernWidget-Skunkworks/Project-Symbiont-LIDAR
+
+License: GNU GPL v3. You should find a copy in the repository.
+******************************************************************************/
+
+#ifndef Apis_h
+#define Apis_h
+
+#include <Arduino.h>
+#include <Wire.h>
+
+#define ADR_DEFAULT 0x50 // Define default address.
+
+// Sensitivity
+#define sensitivityBalanced 0
+#define sensitivityHigh 1
+#define sensitivityLow 2
+#define sensitivityMaxRange 3
+/**
+ * @class Apis Class to Interface with the Apis module for a laser rangefinder.
+ * @brief Arduino library for the Apis board, which manages a LiDAR Lite
+ * unit (roll/pitch, firmware lock/reset, power supply).
+ * @details Library to communicate with the Apis module, which
+ * connects to a LiDAR Lite rangefinder.The Apis is equipped with
+ * capacitors to handle the large burst power draw from the LiDAR Lite, a MEMS
+ * accelerometer to note its orientation, a magnet to note a known orientation
+ * (often, but not necessarily, horizontal)  and the ability to absorb
+ * occasional firmware issues that lead to system hangs.
+ * The leveling helps the user to calculate, for example, a water level
+ * when the sensor is palced on a cliff a tree next to the river but does not
+ * have water below it. The level loses absolute accuracy when near plumb, so
+ * a Hall-effect sensor connected to the magnet allows the user to set a zero
+ * value, thereby correcting for this. Managing failures of the LiDAR Lite
+ * within the Apis is essential, and the Apis therefore acts as a
+ * buffer to protect the data logger from raw sensor failures.
+ *
+ * \verbatim [![DOI](https://zenodo.org/badge/260392682.svg)](https://zenodo.org/badge/latestdoi/260392682) \endverbatim
+ */
+class Apis
+{
+    public:
+        /**
+         * @brief Instantiate Apis object
+         */
+        Apis();
+
+        /**
+         * @brief Begin communications with the Apis using a prescribed
+         * address.
+         * @param Addresss_ default 0x50
+         * DOES NOT YET USE A VARIABLE ADDRESS!
+         * DOES NOT MATTER WHAT YOU WRITE HERE.
+         * THIS DEFAULT ADDRESS CURRENTLY CLASHES WITH HAAR'S DEFAULT!
+         * @param[in] sensitivity. Options are:
+         * sensitivityBalanced (0)
+         * sensitivityHigh (1)
+         * sensitivityLow (2)
+         * sensitivityMaxRange (3)
+         * Default value: sensitivityBalanced (0)
+         * THIS IS ALSO NOT YET IMPLEMENTED IN FIRMWARE! JUST STAYS AT DEFAULT
+         * (SENSITIVITY_BALANCED = 0)
+         * CONSIDER A SET_SENSITIVITY FUNCTION IN THE FUTURE TO ALLOW THE
+         * SENSOR TO AUTOMATICALLY ADJUST THE SENSITIVITY TO RECEIVE THE BEST
+         * POSSIBLE RETURN
+         */
+        void begin(uint8_t Address_ = ADR_DEFAULT, \
+                   uint8_t sensitivity = sensitivityBalanced);
+
+        /**
+         * @brief Measure range [cm], roll [deg] and pitch [deg].
+         * Returns "0" if sensors read error values; else returns "1".
+         */
+        bool updateMeasurements();
+
+        /**
+         * @brief Return range [cm]
+         */
+        float getRange();
+
+        /**
+         * @brief Return roll [deg]
+         */
+        float getRoll();
+
+        /**
+         * @brief Return pitch [deg]
+         */
+        float getPitch();
+
+        /**
+         * @brief Return a comma-separated header:
+         * "Range [cm],Pitch [deg],Roll [deg],"
+         */
+        String getHeader();
+
+        /**
+         * @brief Return comma-separated data values
+         * @details This is the most likely function (alongside getHeader) for
+         * an end user to use.
+         * Values are returned as:
+         * String(Range) + "," + String(Pitch) + "," + String(Roll) + ","
+         * Error values are "-9999" (sensor error) and "-9998" (no measurement
+         * yet taken")
+         * @param[in] takeNewReadings: if `true` run `updateMeasurements` before
+         * returning values. Otherwise, just return values.
+         */
+        String getString(bool takeNewReadings = true);
+
+        /**
+         * @brief CapitalCase version of getHeader() for backwards compatibility
+         */
+        String GetHeader();
+
+        /**
+         * @brief CapitalCase version of getString() for backwards compatibility
+         * @param[in] takeNewReadings: if `true` run `updateMeasurements` before
+         * returning values. Otherwise, just return values.
+         */
+        String GetString(bool takeNewReadings = true);
+
+    private:
+        //Default address
+        uint8_t ADR = ADR_DEFAULT;
+
+        // Variables to save and make accessible to other functions
+        // Initialize at a non-error value; just one that indicates that
+        // they are unset (will be returned if requested before measurement
+        // is taken)
+        float Range = -9998;
+        float Pitch = -9998;
+        float Roll = -9998;
+
+        // Sensor sensitivity; set initially to default "balanced" mode
+        uint8_t sensitivity = sensitivityBalanced;
+};
+
+#endif
